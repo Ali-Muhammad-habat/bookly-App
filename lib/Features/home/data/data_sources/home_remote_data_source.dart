693bcad5e6635e9bc2e933181d619814ba80d6b8
Ali-Muhammad-habat <alihabateng@gmail.com>
2025-07-14 17:21:31 +0300

    feat: Implement HomeRemoteDataSource and ApiService for fetching books from Google Books API

---
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,38 @@
+import 'package:bookly_app/Features/home/data/model/book_model.dart';
+import 'package:bookly_app/Features/home/domain/entity/book_entity.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
+abstract class HomeRemoteDataSource {
+ Future<List<BookEntity>> fetchFeaturedBook();
+ Future<List<BookEntity>> fetchNewestBook();}
+ class HomeRemoteDataSourceImpl implements HomeRemoteDataSource {
+  final ApiService apiService;
+  HomeRemoteDataSourceImpl({required this.apiService});
+  @override
+  Future<List<BookEntity>> fetchFeaturedBook() async {
+ 
+ var data=  await apiService.get(endPoint: "volumes?q=programming&Filtering=free-ebooks");
+ List<BookEntity> books = getBooksList(data);
+ return books;
+
+  }
+
+
+  @override
+  Future<List<BookEntity>> fetchNewestBook() async {
+    var data= await apiService.get(endPoint: "volumes?q=programming&Filtering=free-ebooks&sorting=newest");
+  List<BookEntity> books = getBooksList(data);
+ return books;
+  }
+
+ 
+
+ }
+   List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books=[];
+    for(var  bookMap in data['items']){
+     books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
+  
+  }
+  
